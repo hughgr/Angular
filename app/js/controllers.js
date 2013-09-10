9ff8837dbc1bd6fe0f51fcc9d035f436faf593f8
hughgr <hughgr2006@gmail.com>
2013-09-11 00:22:09 +0800

    add new

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,19 @@
-var phoneListController = function($scope, $http){
+var phoneListController = function($scope, $http,$timeout){
     $http({method:'GET',url:'phones/phones.json'}).success(function(data){
         $scope.phones = data;
     });
     $scope.orderProp = 'age';
     $scope.hello = 'hello world';
+    $timeout(function(){
+            $scope.phones.unshift({
+            
+        "age": -1, 
+        "id": "fuck", 
+        "imageUrl": "img/phones/motorola-xoom-with-wi-fi.0.jpg", 
+        "name": "fuckfuck", 
+        "snippet": "fuckfuck"
+            })
+    },2000)
 
 }
 //phoneListController.$inject = ['$scope','$http'];
